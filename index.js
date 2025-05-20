15f1df586d0561e15cd07b16f8b7c856caec5bbb
Krish A. Patel <krishpatel2067@gmail.com>
2025-05-20 16:22:05 -0400

    Use Express

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-let http = require('http');
-let url = require('url');
+let express = require('express');
+let app = express();
 
-let server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello from the other side!!');
-    res.write(req.url);
-    let q = url.parse(req.url, true).query;
-    let text = q.year + ' ' + q.month;
-    res.end(text);
-});
-
-server.listen(8080);
+app.listen(8080, () => console.log('listening at 8080'));
+app.use(express.static('public'))
