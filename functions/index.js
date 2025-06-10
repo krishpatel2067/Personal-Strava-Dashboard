125aac79fbea36f9da24c27ca03bf379822a1b00
Krish A. Patel <krishpatel2067@gmail.com>
2025-06-10 11:14:24 -0400

    Have trouble with getting emulator to work with admin lol

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,9 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const { initializeApp, cert, storage } = require('firebase-admin/app');
+const fs = require('fs');
+const admin = require('firebase-admin');
+const { initializeApp, cert } = require('firebase-admin/app');
 const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const { getStorage, getDownloadURL } = require("firebase-admin/storage");
@@ -17,18 +19,22 @@ require('dotenv').config();
 // https://firebase.google.com/docs/functions/get-started
 
 const serviceAccount = require('./serviceAccountKey.json');
-initializeApp({
+admin.initializeApp({
     credential: cert(serviceAccount),
     storageBucket: process.env.STORAGE_BUCKET
 });
+// admin.initializeApp();
 
 exports.helloWorld = onRequest((request, response) => {
     logger.info("Reading storage...", { structuredData: true });
-    const bucket = getStorage().bucket();
+    const bucket = admin.storage().bucket();
     const fileRef = bucket.file('test/MyTestNote.txt');
-    getDownloadURL(fileRef).then(url => {
-        logger.info('MyTestNote.txt URL: ', url);
-    });
+    fileRef.download().then(data => {
+        logger.info(data[0]);
+    })
+    // logger.info(fileRef.exists().then(val => {
+    //     logger.info('Done + ' + val);
+    // }));
     response.send("Hello from Firebase!");
 });
 
