b338e2c252d9fa7a3489a7f9fa5df7cc51baa7ab
Krish A. Patel <krishpatel2067@gmail.com>
2025-06-10 10:50:04 -0400

    Test accessing Storage from Functions with admin access

--- functions/index.js
+++ functions/index.js
@@ -7,14 +7,28 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
+const { initializeApp, cert, storage } = require('firebase-admin/app');
 const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const { getStorage, getDownloadURL } = require("firebase-admin/storage");
+require('dotenv').config();
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
+const serviceAccount = require('./serviceAccountKey.json');
+initializeApp({
+    credential: cert(serviceAccount),
+    storageBucket: process.env.STORAGE_BUCKET
+});
+
 exports.helloWorld = onRequest((request, response) => {
-    logger.info("Hello logs!", { structuredData: true });
+    logger.info("Reading storage...", { structuredData: true });
+    const bucket = getStorage().bucket();
+    const fileRef = bucket.file('test/MyTestNote.txt');
+    getDownloadURL(fileRef).then(url => {
+        logger.info('MyTestNote.txt URL: ', url);
+    });
     response.send("Hello from Firebase!");
 });
 
