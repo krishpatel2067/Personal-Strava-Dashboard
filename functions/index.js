4015bda70a94a2dd9c16f263735efea2d0bd863e
Krish A. Patel <krishpatel2067@gmail.com>
2025-06-11 14:37:36 -0400

    Test environment variables with Functions

--- functions/index.js
+++ functions/index.js
@@ -2,14 +2,18 @@ const { initializeApp } = require("firebase-admin/app");
 const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const { getStorage, getDownloadURL } = require("firebase-admin/storage");
-require("dotenv").config();
+const { defineString } = require('firebase-functions/params');
+// require("dotenv").config();
+
+// env vars
+const testVar = defineString("TEST_VAR");
 
 // const serviceAccount = require("./serviceAccountKey.json");
 initializeApp();
 
 exports.helloWorld = onRequest(async (request, response) => {
     logger.info("Reading storage...", { structuredData: true });
-
+    logger.info(testVar.value());
     const bucket = getStorage().bucket();
     logger.info("after bucket");
     const fileRef = bucket.file("test/MyTestNote.txt");
