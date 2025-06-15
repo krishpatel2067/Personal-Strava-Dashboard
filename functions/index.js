af980c214abeb627caada116c69a7f7b06e9a33d
Krish A. Patel <krishpatel2067@gmail.com>
2025-06-15 17:18:23 -0400

    JS Function works with 2 files

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { onRequest } = require("firebase-functions/v2/https");
-const { getStorage, getDownloadURL } = require("firebase-admin/storage");
 const { defineString } = require("firebase-functions/params");
 const logger = require("firebase-functions/logger");
+const test = require("./test");
 
 // env vars
 const testVar = defineString("TEST_VAR");
@@ -16,16 +16,6 @@ const app = initializeApp({
 exports.helloWorld = onRequest(async (request, response) => {
     logger.info("Reading storage...", { structuredData: true });
     logger.info(testVar.value());
-    const bucket = getStorage(app).bucket(bucketVar.value());
-    logger.info("after bucket");
-    const fileRef = bucket.file("test/MyTestNote.txt");
-    const fileRef3 = bucket.file("test/MyTestNote3.txt");
-    logger.info("after fileRef");
-    const url = await getDownloadURL(fileRef);
-    const url3 = await getDownloadURL(fileRef3);
-    logger.info("URL received!");
-    logger.info(url);
-    logger.info(url3);
-
+    test.testFunc(app, bucketVar.value());
     response.send("Hello from Firebase!");
 });
