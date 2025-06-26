43d6ea87863fb7d50106815d9b7bf5246bc04583
Krish A. Patel <krishpatel2067@gmail.com>
2025-06-26 09:10:56 -0400

    Add data and auth files to ignore (fixes auto-refresh issue); add 429 (too many requests) check before storing to storage

--- functions/fetch.js
+++ functions/fetch.js
@@ -80,8 +80,13 @@ async function fetchData(perPage = 1, page = 1, showExpDateMsg = true) {
         }
     });
 
-    const data = await response.json();
-    return data;
+    // 429 - Too many requests
+    if (response.status === 429) {
+        return [{status: 429}, {}];
+    } else {
+        const data = await response.json();
+        return [{status: response.status}, data];
+    }
 }
 
 async function retrieveAllData(app, bucketName, forceNew = false) {
@@ -136,7 +141,9 @@ async function retrieveAllData(app, bucketName, forceNew = false) {
 
         // fetch all data to conserve API requests
         const perPage = MAX_PER_PAGE;
-        const maxPages = 4;            // -1 means all the pages that exist
+        const maxPages = -1;            // -1 means all the pages that exist
+
+        // TODO: EMULATOR REFRESHES WITH JSON EDITS, BREAKS STORING DATA.JSON
 
         let newData = [];
         let tempData = null;
@@ -166,7 +173,15 @@ async function retrieveAllData(app, bucketName, forceNew = false) {
             if (maxPages > 0 && page > maxPages)
                 break;
 
-            tempData = await fetchData(perPage, page, showExpDateMsg);
+            const [response, dataJson] = await fetchData(perPage, page, showExpDateMsg);
+
+            if (response.status === 429) {
+                logger.log("Status code 429 - too many requests. Aborting fetch...");
+                return;
+            } else {
+                tempData = dataJson;
+            }
+
             lastFetched = Date.now();
             page++;
             numFetchesNow++;
