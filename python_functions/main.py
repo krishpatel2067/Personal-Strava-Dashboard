8c831204172be9ebc246a27eaf45958563d7480d
Krish A. Patel <krishpatel2067@gmail.com>
2025-07-01 15:04:43 -0400

    Test Python function

--- python_functions/main.py
+++ python_functions/main.py
@@ -4,11 +4,14 @@
 
 from firebase_functions import https_fn, logger
 from firebase_admin import initialize_app
+import pandas as pd
 
 # initialize_app()
 #
 #
 @https_fn.on_request()
 def on_request_example(req: https_fn.Request) -> https_fn.Response:
+    df = pd.DataFrame({'Column1': [1, 2, 3], 'Column2': [4, 5, 6]})
+    logger.info(df.to_json())
     logger.info("Got request!")
     return https_fn.Response("Hello world from Python!")
