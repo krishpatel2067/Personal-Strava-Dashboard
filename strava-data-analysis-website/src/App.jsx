867def1e63eeb418b868c758d823376e88f32730
Krish A. Patel <krishpatel2067@gmail.com>
2025-07-22 16:13:37 -0400

    Start trying to access storage from the client side

--- strava-data-analysis-website/src/App.jsx
+++ strava-data-analysis-website/src/App.jsx
@@ -1,11 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
 import './App.css'
+import { initializeApp } from 'firebase/app';
+import { getDownloadURL, getStorage, ref } from 'firebase/storage';
+
+const firebaseConfig = {
+  
+};
+
+const app = initializeApp(firebaseConfig);
 
 function App() {
 
+  useEffect(() => {
+    const fetchAnalysis = () => {
+      const storage = getStorage(app);
+      const analysisRef = ref(storage, "public/analysis.json");
+
+      getDownloadURL(analysisRef)
+        .then(async url => {
+          const res = await fetch(url);
+          const json = await res.json();
+          console.log(json);
+        })
+        .catch(err => {
+          console.log("Error while fetching analysis.json:", err.message);
+        });
+    }
+    fetchAnalysis();
+  }, []);
+
   return (
     <div className="App">
-      
+
     </div>
   )
 }
