d51352b39c2eaafb195d0e49b2c3f0e8fc0467b1
Krish A. Patel <krishpatel2067@gmail.com>
2025-07-23 21:44:06 -0400

    Fix CORS issue; unignore package.json and package-lock.json files CORS issue - turns out that my env file was misformatted (had quotes and commas from the JSON copy-paste) :/

--- strava-data-analysis-website/src/App.jsx
+++ strava-data-analysis-website/src/App.jsx
@@ -4,7 +4,13 @@ import { initializeApp } from 'firebase/app';
 import { getDownloadURL, getStorage, ref } from 'firebase/storage';
 
 const firebaseConfig = {
-  
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_ID,
+  measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -15,9 +21,12 @@ function App() {
     const fetchAnalysis = () => {
       const storage = getStorage(app);
       const analysisRef = ref(storage, "public/analysis.json");
+      console.log(analysisRef);
 
       getDownloadURL(analysisRef)
         .then(async url => {
+          console.log(url);
+          
           const res = await fetch(url);
           const json = await res.json();
           console.log(json);
