6f3a3bb10dc5d523a6ccde609d5177f30bf001f3
Krish A. Patel <krishpatel2067@gmail.com>
2025-08-11 17:48:58 -0400

    Add editor config file

--- strava-data-analysis-website/src/components/StatCard.jsx
+++ strava-data-analysis-website/src/components/StatCard.jsx
@@ -1,16 +1,16 @@
 import './StatCard.css';
 
 function StatCard({ name, stat, units, loaded, round = true }) {
-    if (loaded) {
-        return (
-            <div className="StatCard">
-                <span className="name">{name}</span>
-                <br />
-                <span className="stat">{(round ? Math.round(stat) : stat).toLocaleString()}</span>
-                <span className="units"> {units}</span>
-            </div>
-        );
-    }
+  if (loaded) {
+    return (
+      <div className="StatCard">
+        <span className="name">{name}</span>
+        <br />
+        <span className="stat">{(round ? Math.round(stat) : stat).toLocaleString()}</span>
+        <span className="units"> {units}</span>
+      </div>
+    );
+  }
 }
 
 export default StatCard;
