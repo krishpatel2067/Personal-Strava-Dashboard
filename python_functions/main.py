da4894dae5ce9d4266f8134731a4569213926c20
Krish A. Patel <krishpatel2067@gmail.com>
2025-08-11 15:26:48 -0400

    Add "created" field for analysis data

--- python_functions/main.py
+++ python_functions/main.py
@@ -1,12 +1,9 @@
-# Welcome to Cloud Functions for Firebase for Python!
-# To get started, simply uncomment the below code or create your own.
-# Deploy with `firebase deploy`
-
 from firebase_functions import logger, scheduler_fn
 from firebase_admin import initialize_app, get_app, storage, credentials
 import pandas as pd
 from numpy import float64 as np_float64, int64 as np_int64
 from datetime import datetime
+import time
 import json
 
 DATA_PATH = "private/data.json"
@@ -21,7 +18,7 @@ except ValueError as err:
     logger.info(str(err))
     app = initialize_app(cred)
 
-def convert_np_types_to_plain(dictionary: dict) -> dict:
+def convert_np_types_to_plain(dictionary):
     for key, value in dictionary.items():
         if isinstance(value, np_float64):
             dictionary[key] = float(value)
@@ -31,7 +28,7 @@ def convert_np_types_to_plain(dictionary: dict) -> dict:
             dictionary[key] = convert_np_types_to_plain(value)
     return dictionary
 
-def analyze(data: dict) -> dict:
+def analyze(data):
     df = pd.DataFrame(data)
     analysis = dict()
 
@@ -52,15 +49,14 @@ def analyze(data: dict) -> dict:
     
     # total elapsed time
     analysis["total_elapsed_time"] = df["elapsed_time"].sum()
-
+    
     # np.int64 or np.float64 are not JSON serializable, so convert them to their plain counterparts
-    return convert_np_types_to_plain(analysis)
+    return convert_np_types_to_plain({ "created": time.time(), "data": analysis })
 
 
 @scheduler_fn.on_schedule(schedule="every day 02:00")
-def read_and_analyze(event: scheduler_fn.ScheduledEvent) -> None:
+def read_and_analyze():
     logger.info("Running Python function `read_and_analyze`...")
-    # logger.info(event)
 
     bucket = storage.bucket(app=app)
     data_blob = bucket.get_blob(DATA_PATH)
@@ -76,7 +72,7 @@ def read_and_analyze(event: scheduler_fn.ScheduledEvent) -> None:
         data = overall_data["data"]
 
         logger.info(f"Successfully loaded {DATA_PATH}, which was last saved {datetime.fromtimestamp(last_saved/1000)}")
-        analysis: dict = analyze(data)
+        analysis = analyze(data)
 
         analysis_blob = bucket.blob(ANALYSIS_PATH)
         analysis_blob.upload_from_string(
