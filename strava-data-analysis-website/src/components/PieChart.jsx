33d4b313f74c355dc7253b37f814c7398c43ed63
Krish A. Patel <krishpatel2067@gmail.com>
2025-08-14 15:39:58 -0400

    Write PieChart code

--- strava-data-analysis-website/src/components/PieChart.jsx
+++ strava-data-analysis-website/src/components/PieChart.jsx
@@ -1,7 +1,19 @@
-import echarts from "echarts";
+import * as echarts from "echarts/core";
+import { BarChart } from "echarts/charts";
+import ReactEchartsCore from "echarts-for-react/lib/core";
 
-function PieChart({}) {
+echarts.use([BarChart]);
 
+function PieChart({ option: optionProp }) {
+  const option = optionProp ?? {};
+
+  return (
+    <ReactEchartsCore
+      echarts={echarts}
+      option={option}
+      style={{ width: "400px", height: "100%" }}
+    />
+  );
 }
 
 export default PieChart;
