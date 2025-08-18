b374a3316b17965636fbd86aded287c42291054d
Krish A. Patel <krishpatel2067@gmail.com>
2025-08-18 14:26:28 -0400

    Finish writing Checkbox code

--- strava-data-analysis-website/src/components/Checkbox.jsx
+++ strava-data-analysis-website/src/components/Checkbox.jsx
@@ -1,5 +1,17 @@
-function Checkbox({}) {
+import { useState } from "react";
 
+function Checkbox({ defaultValue, onChange }) {
+  const [checked, setChecked] = useState(defaultValue ?? false);
+
+  const onClickHandler = () => {
+    const newChecked = !checked;
+    setChecked(newChecked);
+    onChange(newChecked);
+  }
+
+  return (
+    <button className={"Checkbox"} onClick={onClickHandler} data-checked={checked}></button>
+  );
 }
 
 export default Checkbox;
