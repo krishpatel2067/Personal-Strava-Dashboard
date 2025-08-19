487369cb91b5ba6f40d9280f6520779d996a9e4b
Krish A. Patel <krishpatel2067@gmail.com>
2025-08-19 13:51:34 -0400

    Implement tap outside to hide Tooltip content for mobile

--- strava-data-analysis-website/src/components/Tooltip.jsx
+++ strava-data-analysis-website/src/components/Tooltip.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
+import { useDetectClickOutside } from "react-detect-click-outside";
 import "./Tooltip.css";
 
 function Tooltip({ content }) {
   const [visible, setVisible] = useState(false);
+  const ref = useDetectClickOutside({ onTriggered: () => setVisible(false) });
 
   return (
     <div className="Tooltip">
-      <button className="activator" onClick={() => setVisible(prev => !prev)}>?</button>
+      <button className="activator" ref={ref} onClick={() => setVisible(true)}>
+        ?
+      </button>
       <div className="content" data-visible={visible}>
         {content}
       </div>
