e3d1058fd5415014cab95e8128099dc76c997ed5
Krish A. Patel <krishpatel2067@gmail.com>
2025-08-19 12:33:22 -0400

    Write Textbox code

--- strava-data-analysis-website/src/components/Textbox.jsx
+++ strava-data-analysis-website/src/components/Textbox.jsx
@@ -1,5 +1,28 @@
-function Textbox({}) {
+import { useState } from "react";
 
+function Textbox({ defaultValue }) {
+  const [input, setInput] = useState(defaultValue ?? "");
+
+  const onChangeWrapper = (event) => {
+    event.preventDefault();
+    const newInput = event.target.value;
+    setInput(newInput);
+
+    if (onChange != null) {
+      onChange(newInput);
+    }
+  }
+
+  return (
+    <div className="Textbox">
+      <input
+        type="text"
+        value={input}
+        onChange={onChangeWrapper}
+      />
+      {/* tool tip */}
+    </div>
+  );
 }
 
 export default Textbox;
